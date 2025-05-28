25076737aae0ee698d3597ed2b2c40480c970c15
dash44 <d.naumova@connect.qut.edu.au>
2025-05-28 11:19:57 +1000

    contact javadoc comments

--- src/main/java/com/example/aiconceptsexplorer/models/Contact.java
+++ src/main/java/com/example/aiconceptsexplorer/models/Contact.java
@@ -1,5 +1,9 @@
 package com.example.aiconceptsexplorer.models;
 
+/**
+ * Represents a contact with personal info such as name, email, and phone number.
+ * Provides methods to access and modify contact details.
+ */
 public class Contact {
     private int id;
     private String firstName;
@@ -7,6 +11,13 @@ public class Contact {
     private String email;
     private String phone;
 
+    /**
+     * Constructs a new Constact with the specified details.
+     * @param firstName the first name of the contact
+     * @param lastName the last name of the contact
+     * @param email the email address of the contact
+     * @param phone the phone number of the contact
+     */
     public Contact(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -14,46 +25,90 @@ public class Contact {
         this.phone = phone;
     }
 
+    /**
+     * Returns the unique identifier of the contact.
+     * @return the contact ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the unique identifier for the contact.
+     * @param id the contact ID to set
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Returns the first name of the contact.
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name of the contact.
+     * @param firstName the first name to set
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns the last name of the contact.
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name of the contact.
+     * @param lastName the last name to set
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns the email address of the contact.
+     * @return the email address
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email address of the contact.
+     * @param email the email address to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Returns the phone number of the contact.
+     * @return the phone number
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Sets the phone number of the contact.
+     * @param phone the phone number to set
+     */
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
+    /**
+     * Returns the full name of the contact by combining first and last names.
+     * @return the full name of the contact
+     */
     public String getFullName() {
         return firstName + " " + lastName;
     }
